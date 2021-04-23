80d0d0caeb839faaf40ebc18bdc496583d3acbe8
Preston Lamb <preston.j.lamb@gmail.com>
2021-04-22 23:34:45 -0600

    added lifecycle hooks and host listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'lightning-exit-code';
+export class AppComponent implements OnInit, OnDestroy {
+	title = 'lightning-exit-code';
+
+	@HostListener('window:unload', ['$event'])
+	unloadHandler(event: Event) {
+		console.log('Hello from unloadHandler, ', event);
+	}
+
+	@HostListener('window:beforeunload', ['$event'])
+	beforeUnloadHandler(event: Event) {
+		console.log('Hello from beforeUnloadHandler, ', event);
+	}
+
+	ngOnInit() {
+		console.log('Hello from ngOnInit');
+	}
+
+	ngOnDestroy() {
+		console.log('Hello from ngOnDestroy');
+	}
 }
